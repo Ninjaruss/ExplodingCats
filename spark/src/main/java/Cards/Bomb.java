ff02151aefdbe48e8206654452a8cc6a893a6e11
Yanrui Xu <amandax2013.ax@gmail.com>
2019-12-04 19:56:31 -0800

    modified description

--- spark/src/main/java/Cards/Bomb.java
+++ spark/src/main/java/Cards/Bomb.java
@@ -4,7 +4,7 @@ public class Bomb extends CardObject{
     public Bomb(){
         name = "Bomb";
         id = 1;
-        desc = "When this card is drawn, you lose.";
+        desc = "When this card is drawn, you lose if no defuse card is played.";
     }
 
     public void onDraw(){
--- spark/src/main/java/Cards/Defuse.java
+++ spark/src/main/java/Cards/Defuse.java
@@ -4,6 +4,6 @@ public class Defuse extends CardObject{
     public Defuse(){
         name = "Defuse";
         id = 2;
-        desc = "Disables a 'Bomb' card.";
+        desc = "Disables a 'Bomb' card and put the 'bomb' card back in the deck.";
     }
 }
