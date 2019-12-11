7a316977bcd03f3cdc8392e9ab56dade5fe4b22a
Ninjaruss <45620071+Ninjaruss@users.noreply.github.com>
2019-12-10 17:12:39 -0800

    Added wins and session fields.

--- spark/src/main/java/DTO/User.java
+++ spark/src/main/java/DTO/User.java
@@ -1,4 +1,5 @@
 package DTO;
+import org.eclipse.jetty.websocket.api.Session;
 
 import java.util.*;
 
@@ -6,10 +7,21 @@ public class User {
     public final String _id;
     public final String name;
     public final Date date;
+    public final int wins;
+    private Session session;
 
-    public User(String _id, String name, Date date){
+    public User(String _id, String name, Date date, int wins){
         this._id = _id;
         this.name = name;
         this.date = date;
+        this.wins = wins;
+    }
+
+    public void setSession(Session session){
+        this.session = session;
+    }
+
+    public Session getSession(){
+        return this.session;
     }
 }
