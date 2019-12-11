e81c0092ae9af97c18e5cca43c3398f6da161310
Ninjaruss <45620071+Ninjaruss@users.noreply.github.com>
2019-12-10 17:18:13 -0800

    Main framework established.

--- spark/src/main/java/WebSocketHandler.java
+++ spark/src/main/java/WebSocketHandler.java
@@ -1,16 +1,31 @@
+import GameObjects.Game;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.*;
 
+import DTO.*;
+import DAO.UserService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 @WebSocket
 public class WebSocketHandler {
     // Store sessions if you want to, for example, broadcast a message to all users
     static Map<Session, Session> sessionMap = new ConcurrentHashMap<>();
+
+    // Map of users who are logged in
+    static Map<Session, User> userMap = new ConcurrentHashMap<>();
+
+    // Map of games being hosted
+    static Map<Integer, Game> gameList = new ConcurrentHashMap<>();
+
+    static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    static UserService userService = UserService.getDataBase();
     static String clickCountString = "0";
 
-    public static void broadcast(String message) {
+    public static void broadcastToAllUsers(String message) {
         sessionMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
             try {
                 session.getRemote().sendString(message); // server to client
@@ -18,6 +33,61 @@ public class WebSocketHandler {
                 e.printStackTrace();
             }
         });
+    }
+
+    public static void broadcastToGame(Integer id, String message) {
+        for (Map.Entry<Integer, Game> entry : gameList.entrySet()){
+            if (entry.getValue().id == id){
+                List<User> userList = entry.getValue().getPlayerList();
+                for (User u : userList){
+                    try {
+                        u.getSession().getRemote().sendString(message);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void hostGame(User u){
+        Game game = new Game();
+        game.addPlayer(u);
+        gameList.put(game.id, game);
+    }
+
+    // Look for an open game to join, if none found make new game
+    public static void findMatch(User u){
+        for (Map.Entry<Integer, Game> entry : gameList.entrySet()){
+            if (entry.getValue().getStatus() == Game.Status.WAITING){
+                entry.getValue().addPlayer(u);
+
+                Response newResponse = new Response.Builder()
+                        .setCommand("foundGame")
+                        .setGameResponse(entry.getValue())
+                        .setCode("User has found and joined a game.")
+                        .build();
+                try {
+                    u.getSession().getRemote().sendString(gson.toJson(newResponse));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return;
+            }
+        }
+        hostGame(u);
+    }
+
+    public static void startGame(Integer id){
+        for (Map.Entry<Integer, Game> entry : gameList.entrySet()){
+            if (entry.getKey() == id){
+                entry.getValue().startGame();
+            }
+        }
+    }
+
+    public static void playCard(Integer id){
+
     }
 
     @OnWebSocketConnect
@@ -35,8 +105,32 @@ public class WebSocketHandler {
 
     @OnWebSocketMessage
     public void message(Session session, String message) throws IOException {
-        System.out.println("Got: " + message);   // Print message
-        clickCountString = message; // save the count
-        broadcast(message);
+        Response response = gson.fromJson(message, Response.class);
+
+        // connectUser("username") - Create new User if username isn't in the database, returns a user
+        if (response.getCommand() == "connectUser"){
+            User u = userService.getUser(response.getStringResponse());
+            Response.Builder newResponse = new Response.Builder();
+
+            if (u != null){
+                        newResponse.setCode("User with name found! Data fetched.");
+            } else {
+                u = userService.addUser(response.getStringResponse());
+                newResponse.setCode("New user created!");
+            }
+
+            u.setSession(session); // Sets the user's session
+            userMap.put(session, u); // Binds session to user
+
+            newResponse.setUserResponse(u);
+            newResponse.setCommand("userConnected");
+            Response finalResponse = newResponse.build();
+
+            // Return a response message to the client
+            session.getRemote().sendString(gson.toJson(finalResponse));
+            findMatch(u);
+        }
+
+        // playCard(GameObjects.Game id, String cardname)
     }
 }
